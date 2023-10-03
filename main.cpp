51df30a0657bbe2f7f94fa53ba2606513c2c214a
Coopus <120044030+CoopusDev@users.noreply.github.com>
2023-10-03 20:03:03 +0800

    fixed a little bug

--- main.cpp
+++ main.cpp
@@ -23,10 +23,10 @@ Eigen::Matrix4f get_model_matrix(float rotation_angle)
 {
     Eigen::Matrix4f model = Eigen::Matrix4f::Identity();
 
-    model(0, 0) = cos(rotation_angle);
-    model(1, 0) = -sin(rotation_angle);
-    model(0, 1) = sin(rotation_angle);
     model(1, 1) = cos(rotation_angle);
+    model(2, 1) = -sin(rotation_angle);
+    model(1, 2) = sin(rotation_angle);
+    model(2, 2) = cos(rotation_angle);
 
     return model;
 }
@@ -51,29 +51,15 @@ Eigen::Matrix4f get_projection_matrix(float eye_fov, float aspect_ratio,
     return projection.transpose();
 }
 
-int main(int argc, const char **argv)
+int main()
 {
     float angle = 0;
-    bool command_line = false;
-    std::string filename = "output.png";
-
-    if (argc >= 3)
-    {
-        command_line = true;
-        angle = std::stof(argv[2]); // -r by default
-        if (argc == 4)
-        {
-            filename = std::string(argv[3]);
-        }
-        else
-            return 0;
-    }
 
     rst::rasterizer r(700, 700);
 
     Eigen::Vector3f eye_pos = {0, 0, 5};
 
-    std::vector<Eigen::Vector3f> pos{{2, 0, -2}, {0, 2, -2}, {-2, 0, -2}};
+    std::vector<Eigen::Vector3f> pos{{2, 1, -1}, {1, 2, -2}, {-2, 0, -2}};
 
     std::vector<Eigen::Vector3i> ind{{0, 1, 2}};
 
@@ -82,23 +68,6 @@ int main(int argc, const char **argv)
 
     int key = 0;
     int frame_count = 0;
-
-    if (command_line)
-    {
-        r.clear(rst::Buffers::Color | rst::Buffers::Depth);
-
-        r.set_model(get_model_matrix(angle));
-        r.set_view(get_view_matrix(eye_pos));
-        r.set_projection(get_projection_matrix(45, 1, 0.1, 50));
-
-        r.draw(pos_id, ind_id, rst::Primitive::Triangle);
-        cv::Mat image(700, 700, CV_32FC3, r.frame_buffer().data());
-        image.convertTo(image, CV_8UC3, 1.0f);
-
-        cv::imwrite(filename, image);
-
-        return 0;
-    }
 
     while (key != 27)
     {
